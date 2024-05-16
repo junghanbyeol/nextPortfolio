93988781b474280841c6c3723569d2b633a1f0ef
gksquf9720 <gksquf9720@naver.com>
2024-05-16 15:19:58 +0900

    다크모드 dark class 방식 수정

--- components/dark-mode-toggle-button.js
+++ components/dark-mode-toggle-button.js
@@ -1,29 +1,32 @@
 import { useTheme } from 'next-themes'
 
-
 export default function DarkModeToggleButton(){
     const { theme, setTheme } = useTheme()
     return(
         <>
-            <button className="inline-flex items-center 
+            <button className="
+            inline-flex items-center 
             bg-gray-100 border-0 py-1 px-3
-            focus:outline-none 
+            focus:outline-none
+            bg-gray-100
             hover:bg-gray-200 
-            hover:text-red-500 
-            rounded text-base mt-4 md:mt-0"
+            hover:text-orange-500
+            "
                 type="button"
                 onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
             >
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" 
-            className="visible dark:invisible dark:h-0 dark:w-0 h-5 w-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+            {/* Light Mode Icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${theme === 'dark' ? 'invisible h-0 w-0' : 'visible h-6 w-6'}`}>
+                <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
             </svg>
 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
-            className="invisible dark:visible dark:h-5 dark:w-5 h-0 w-0">
+            {/* Dark Mode Icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${theme === 'dark' ? 'visible h-6 w-6' : 'invisible h-0 w-0'}`}>
                 <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
             </svg>
+
+
 
             </button>
         </>
